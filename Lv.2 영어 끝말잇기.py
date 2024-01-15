edddc9b7c49109db1835e5adfa8fb33c76498e9b
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-01-15 22:21:24 +0900

    update

---
+++ Lv.2 영어 끝말잇기.py
@@ -0,0 +1,18 @@
+def solution(n, words):
+    answer = []
+    _list = []
+    # num: 번호, order: 차례
+    num, order = 0, 1
+    count = 0
+    for i in words:
+        if num == n: 
+            num = 0
+            order += 1
+        num += 1
+        count += 1
+        if (count > 1 and _list[-1][-1] != i[0]) or (count > 1 and i in _list) or len(i) == 1: # 끝말잇기 안되는 경우, 이미 나왔던 단어인 경우, 한 글자인 단어인 경우
+            answer.extend([num, order])
+            break
+        elif len(words) == count: answer.extend([0, 0])
+        _list.append(i)
+    return answer
---
+++ Lv.2 점프와 순간 이동.py
@@ -0,0 +1,12 @@
+# case1: K 칸을 앞으로 점프 (k칸마다 건전지가 k만큼 닮)
+# case2: 현재까지 온 거리 x 2에 해당하는 위치로 순간이동 (건전지 안듦)
+
+def solution(n):
+    ans = 0
+    _list = []
+    count = 0
+    for i in range(n):
+        
+    
+
+    return ans
