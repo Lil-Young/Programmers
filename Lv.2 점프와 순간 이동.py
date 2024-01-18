7b0d71c61af3a21836c600c96871be25a748f24e
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-01-18 20:34:03 +0900

    update

--- Lv.2 점프와 순간 이동.py
+++ Lv.2 점프와 순간 이동.py
@@ -1,18 +1,16 @@
 # case1: K 칸을 앞으로 점프 (k칸마다 건전지가 k만큼 닮)
 # case2: 현재까지 온 거리 x 2에 해당하는 위치로 순간이동 (건전지 안듦)
+import math
 
 def solution(n):
-    ans = 0
-    count = 1 # 만드려는 n
-    jump_count = 0 # jump 카운트
-    k_count = 1 # k 카운트
+    count = 1
     while True:
-        count *= 2
-        if n < count:
-            k_count += (n-(count//2))
-            ans = k_count
-            break
-    return ans
-
-result = solution(5000)
+        if n == 1:
+            return count
+        n = n/2
+        f_n = (n%1)*10
+        if f_n == 5:
+            n = math.floor(n)
+            count+=1
+result = solution(1)
 print(result)
