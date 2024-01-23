a3cc57b9a4b7b786edf81847966ab4ef9fc22f02
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-01-23 23:55:43 +0900

    update

---
+++ Lv.1 가장 많이 받은 선물.py
@@ -0,0 +1,46 @@
+def solution(friends: list, gifts: list):
+    answer = [0] * len(friends)  # 정답 리스트
+    index_list = []  # +=1 을 하기 위한 인덱스
+    # 준사람/받은사람 리스트, answer 리스트를 0으로 초기화
+    _list = [[0] * len(friends) for _ in range(len(friends))]
+
+    for i in range(len(friends)):
+        for j in range(len(friends)):
+            if i != j:
+                index_list.extend([[i, j]])
+
+    set_index_list = list(set([tuple(sorted(index_list)) for index_list in index_list]))  # 중복제거
+
+    # 준사람 선물 추가
+    for i in gifts:
+        spl_str = i.split()
+        a = friends.index(spl_str[0])
+        b = friends.index(spl_str[1])
+        _list[a][b] += 1
+
+    # 선물 지수
+    futures_list = []
+    for i, j in enumerate(_list):
+        have_sum = 0
+        for k in range(len(friends)):
+            have_sum += _list[k][i]
+        futures_list.append(sum(j) - have_sum)
+
+    # answer에 추가
+    for i in set_index_list:
+        if _list[i[0]][i[1]] == _list[i[1]][i[0]] or (_list[i[0]][i[1]] == 0 and _list[i[1]][i[0]] == 0):
+            if futures_list[i[0]] > futures_list[i[1]]:
+                answer[i[0]] += 1
+            elif futures_list[i[0]] < futures_list[i[1]]:
+                answer[i[1]] += 1
+        else:
+            if _list[i[0]][i[1]] > _list[i[1]][i[0]]:
+                answer[i[0]] += 1
+            else:
+                answer[i[1]] += 1
+    result = max(answer)
+    return result
+
+if __name__ == "__main__":
+    result = solution(["a", "b", "c"], ["a b", "b a", "c a", "a c", "a c", "c a"])
+    print(result)
--- Lv2. 예상 대진표.py
+++ Lv2. 예상 대진표.py
@@ -1,11 +1,9 @@
-import math
 def solution(n,a,b):
     answer = 0
-    while True:
-        if a == b:
-            break
-        a = a//2 + a%2
-        b = b//2 + b%2
+    a, b = a-1, b-1
+    while a != b:
+        a = a//2
+        b = b//2
         answer+=1
     return answer
 
