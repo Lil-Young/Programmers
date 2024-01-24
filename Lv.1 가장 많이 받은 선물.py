9fb79a38e0cdcf849b421196d6a1f4737bd909ae
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-01-24 23:53:45 +0900

    update

--- Lv.1 가장 많이 받은 선물.py
+++ Lv.1 가장 많이 받은 선물.py
@@ -1,15 +1,15 @@
 def solution(friends: list, gifts: list):
-    answer = [0] * len(friends)  # 정답 리스트
-    index_list = []  # +=1 을 하기 위한 인덱스
-    # 준사람/받은사람 리스트, answer 리스트를 0으로 초기화
-    _list = [[0] * len(friends) for _ in range(len(friends))]
+    answer = [0] * len(friends)  # 각각 선물을 몇번 받는지에 관한 리스트
+    index_list = []  # 비교를 하기 위한 index
+    _list = [[0] * len(friends) for _ in range(len(friends))] # 준사람/받은사람 2차원 리스트
 
+    # 비교하기 위한 인덱스 리스트 생성
     for i in range(len(friends)):
         for j in range(len(friends)):
             if i != j:
                 index_list.extend([[i, j]])
 
-    set_index_list = list(set([tuple(sorted(index_list)) for index_list in index_list]))  # 중복제거
+    set_index_list = list(set([tuple(sorted(index_list)) for index_list in index_list]))  # 인덱스 중복제거
 
     # 준사람 선물 추가
     for i in gifts:
