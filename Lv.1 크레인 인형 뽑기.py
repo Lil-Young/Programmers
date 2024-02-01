fdacc7bea3e90af9561122ac889ceebdb3adf5cc
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-02-01 23:39:47 +0900

    update

--- Lv.1 크레인 인형 뽑기.py
+++ Lv.1 크레인 인형 뽑기.py
@@ -1,17 +1,29 @@
 def solution(board:list, moves:list):
-    answer = 0
-    n_list = []
-    cpy_board = board
+    answer = 0 # 정답
+    n_list = [] # 크레인으로 뽑은 인형 리스트
+
+    # 크레인으로 인형 뽑기
     for i in range(len(moves)):
         for j in range(len(board)):
             if (board[j][moves[i]-1]) != 0:
                 n_list.append((board[j][moves[i]-1]))
                 board[j][moves[i]-1] = 0
                 break
-    print(n_list)
 
+    # 뽑은 인형 리스트에서 같은 인형이면 해당 인형 삭제 및 answer += 2
+    before, after = 0, 1
+    while True:
+        if after >= len(n_list): 
+            break
+        if n_list[before] == n_list[after]:
+            del n_list[before:(after+1)]
+            answer+=2
+            before, after = 0, 1
+        else:
+            before += 1
+            after += 1
     return answer
 
 if __name__ == "__main__":
-    result = solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])
+    result = solution([[1, 0, 0, 0, 0], [2, 0, 0, 0, 0], [2, 0, 0, 0, 0], [1, 0, 0, 0, 0], [3, 0, 0, 0, 0]], [1, 1, 1, 1])
     print(result)
