a053f7f7f93a25a93c2e2c4427af828e885fc731
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-02-09 04:01:13 +0900

    update

--- Lv.2 숫자의 표현.py
+++ Lv.2 숫자의 표현.py
@@ -1,19 +1,17 @@
 def solution(n):
-    answer = 1
+    answer = 0
     cnt, sum = 1, 0
-    start, end = 0, n
-    _list = [x for x in range(1, n+1)]
-    for i in range(n//2):
-        for j in range(n//2 + 1):
-            sum += _list[start]
-            start+=1
-            if sum == n:
-                answer+=1
-                break
+    while cnt <= n:
         sum = 0
-        start = 0
-        
-        return answer
+        for i in range(cnt, n+1):
+            sum += i
+            if sum == n:
+                answer += 1
+                break
+            elif sum > n:
+                break
+        cnt += 1
+    return answer
 
 
 if __name__ == "__main__":
