462c9b2b3b9e8ee38af48af63c8d54d27eae73a3
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-02-19 20:59:26 +0900

    update

--- test.py
+++ test.py
@@ -1,39 +1,16 @@
-from collections import deque
+n, k = map(int, input().split()) # N과 K를 입력 받기
+a = list(map(int, input().split())) # 배열 A의 모든 원소를 입력 받기
+b = list(map(int, input().split())) # 배열 B의 모든 원소를 입력 받기
 
-# BFS 소스코드 구현
-def bfs(x, y):
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque()
-    queue.append((x, y))
-    # 큐가 빌 때까지 반복하기
-    while queue:
-        x, y = queue.popleft()
-        # 현재 위치에서 4가지 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-        # 가장 오른쪽 아래까지의 최단 거리 반환
-        return graph[n -1][m - 1]
-# N, M을 공백을 기준으로 구분하여 입력 받기
-n, m = map(int, input().split())
-# 2차원 리스트의 맵 정보 입력 받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+a.sort() # 배열 A는 오름차순 정렬 수행
+b.sort(reverse=True) # 배열 B는 내림차순 정렬 수행
 
-# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# BFS를 수행한 결과 출력
-print(bfs(0, 0))
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+        break
+print(sum(a))
