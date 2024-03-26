bd6b414b95987360aaf380e188bef6da528d6f89
Lil-Young <thddyddls1@naver.com>
2024-03-26 23:19:36 +0900

    update

---
+++ Lv.2 주차요금 계산.py
@@ -0,0 +1,48 @@
+import math
+
+def solution(fees:list, records:list):
+    # fees: 주차요금(기본 시간(분), 기본 요금(원), 단위 시간(분), 단위 요금(원)), records: 자동차의 입/출차 내역
+    answer = []
+    records = [x.split(' ') for x in records]
+    records = sorted(records, key = lambda x:(x[1], x))
+    set_r = list(set(x[1] for x in records))
+    set_r.sort()
+    cars_minute = {x:0 for x in set_r}
+    v = 23*60 + 59
+    stack = []
+    for i in records:
+        to_minute = int(i[0][:2])*60 + int(i[0][3:])
+        if stack and i[2] == "IN":
+            pre_val = stack.pop()
+            cars_minute[pre_val[1]] += (v - pre_val[0])
+
+            stack.append((to_minute, i[1]))
+            continue
+
+        if i[2] == "IN":
+            stack.append((to_minute, i[1]))
+        elif i[2] == "OUT":
+            pre_val = stack.pop()
+            cars_minute[pre_val[1]] += (to_minute - pre_val[0])
+    if stack:
+        pre_val = stack.pop()
+        cars_minute[pre_val[1]] += (v - pre_val[0])
+    for i in cars_minute.values():
+        fee = fees[1] + math.ceil((i - fees[0])/fees[2]) * fees[3]
+        if fee > fees[1]:
+            answer.append(fee)
+        else:
+            answer.append(fees[1])
+    return answer
+# 0000 | 34 + 300 = 334	| 5000 + ⌈(334 - 180) / 10⌉ x 600 = 14600
+
+if __name__ == '__main__':
+    result = solution([180, 5000, 10, 600],
+                      ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
+    print(result)
+    result2 = solution([120, 0, 60, 591],
+                       ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"])
+    print(result2)
+    result3 = solution([1, 461, 1, 10],
+                       ["00:00 1234 IN"])
+    print(result3)
