f860059618c7712492c2001ed06c4f30c9e747a2
Lil-Young <thddyddls1@naver.com>
2024-04-28 23:50:18 +0900

    update

---
+++ Lv.2 타겟 넘버.py
@@ -0,0 +1,10 @@
+def solution(numbers:list, target):
+    answer = 0
+    
+    return answer
+
+if __name__ == '__main__':
+    result = solution([1, 1, 1, 1 ,1], 3)
+    print(result)
+    result2 = solution([4, 1, 2, 1], 4)
+    print(result2)
