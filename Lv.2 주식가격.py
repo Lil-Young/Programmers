51897973e42283a63e29df44490fedf25a769412
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-06-11 00:29:12 +0900

    update

--- Lv.2 주식가격.py
+++ Lv.2 주식가격.py
@@ -1,13 +1,14 @@
 def solution(prices):
-    answer = [0] * len(prices)
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i] += 1
-            else:
-                answer[i] += 1
+    answer = []
+    for idx, price in enumerate(prices):
+        a = 0
+        for i in range(idx+1, len(prices)):
+            a+=1
+            if price > prices[i]:
                 break
+        answer.append(a)
     return answer
+
 
 result = solution([1,2,3,2,3])
 print(result)
