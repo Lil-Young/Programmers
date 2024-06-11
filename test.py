baec8dd4a8f3fdeb4ba83eeb870532f4bc0456ea
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-06-12 06:54:13 +0900

    update

--- test.py
+++ test.py
@@ -1,45 +1,9 @@
-# DFS -> 재귀함수
-# BFS -> deque
-
-def dfs1(x, y):
-    if x < -1 or x >= n or y < -1 or y >= m:
-        return
-    if graph[x][y] == 'check':
-        return
-    if graph[x][y] == '|':
-        return
-    if graph[x][y] == '-':
-        graph[x][y] = 'check'
-        dfs1(x, y+1)
-        return True
-    return False
-
-def dfs2(x, y):
-    if x < -1 or x >= n or y < -1 or y >= m:
-        return
-    if graph[x][y] == 'check':
-        return
-    if graph[x][y] == '-':
-        return
-    if graph[x][y] == '|':
-        graph[x][y] = 'check'
-        dfs2(x+1, y)
-        return True
-    return False
-
-
-
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(input()))
-
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs1(i, j) == True:
-            result += 1
-        if dfs2(i, j) == True:
-            result += 1
-print(result)
-
+import queue
+data = queue.Queue()
+data.put('test')
+data.put(1)
+print(data.qsize())
+print(data.get())
+print(data.qsize())
+print(data.get())
+print(data.qsize())
